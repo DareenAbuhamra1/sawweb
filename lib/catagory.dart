0437fa0cc6f0e2be624e14f971e385595fc8e0c3
Dareen Abuhamra <155165620+DareenAbuhamra1@users.noreply.github.com>
2025-06-03 15:56:49 +0300

    Update catagory.dart

--- lib/catagory.dart
+++ lib/catagory.dart
@@ -17,71 +17,76 @@ class CategoriesGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // صفين: 3 عناصر في كل صف
-        for (int row = 0; row < 2; row++)
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              for (int i = row * 3; i < row * 3 + 3; i++)
-                if (i < categories.length)
-                  Padding( 
-                    padding: EdgeInsets.only(
-                      left: i % 3 == 0 ? 25 : 0,
-                      right: 15,
-                      top: row == 0 ? 25 : 0,
-                      bottom: 15,
-                    ),
-                    child: GestureDetector(
-                      onTap: () {
-                        // عند الضغط ننتقل للصفحة الثانية مع إرسال اسم التصنيف
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder:
-                                (_) => Report(
-                                  categoryName: categories[i]['label'],
-                                ),
-                          ),
-                        );
-                      },
-                      child: Container(
-                        height: 110,
-                        width: 110,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                          border: Border.all(
-                            color: Color.fromARGB(55, 0, 0, 0),
-                            width: 1.4,
-                          ),
+    return Padding(
+     padding: const EdgeInsets.fromLTRB(0, 0, 8, 0),
+      child: Container(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            for (int row = 0; row < 2; row++)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  for (int i = row * 3; i < row * 3 + 3; i++)
+                    if (i < categories.length)
+                      Padding( 
+                        padding: EdgeInsets.only(
+                          left: i % 3 == 0 ? 25 : 0,
+                          right: 15,
+                          top: row == 0 ? 25 : 0,
+                          bottom: 15,
                         ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Icon(
-                              categories[i]['icon'],
-                              size: 40,
-                              color: Color.fromARGB(255, 10, 40, 95),
-                            ),
-                            SizedBox(height: 5),
-                            Text(
-                              categories[i]['label'],
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Color.fromARGB(255, 10, 40, 95),
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder:
+                                    (_) => Report(
+                                      categoryName: categories[i]['label'],
+                                    ),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            height: 110,
+                            width: 110,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(
+                                color: Color.fromARGB(55, 0, 0, 0),
+                                width: 1.4,
                               ),
                             ),
-                          ],
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  categories[i]['icon'],
+                                  size: 40,
+                                  color: Color.fromARGB(255, 10, 40, 95),
+                                ),
+                                SizedBox(height: 5),
+                                Text(
+                                  categories[i]['label'],
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Color.fromARGB(255, 10, 40, 95),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-            ],
-          ),
-      ],
+                ],
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
